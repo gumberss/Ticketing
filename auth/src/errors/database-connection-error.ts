32cff7558c41fcd1844a55941eb0e2d97569f54e
Gumbers <gustavomarinsuppi@gmail.com>
2020-05-21 19:56:09 -0300

    refactor: creation of the abstract class CustomErrors to ensure we always return the same template of errors

--- auth/src/errors/database-connection-error.ts
+++ auth/src/errors/database-connection-error.ts
@@ -1,18 +1,20 @@
-export class DatabaseConnectionError extends Error {
+import { CustomError } from './custom-error'
+
+export class DatabaseConnectionError extends CustomError {
 	statusCode = 500
 	reason = 'Error connection to database'
 	constructor() {
-		super()
+		super('Error connection to db')
 
 		// Only because we are extending a built in class
 		Object.setPrototypeOf(this, DatabaseConnectionError.prototype)
 	}
 
-	serializeError() {
+	serializeErrors() {
 		return [
 			{
 				message: this.reason,
-			}
+			},
 		]
 	}
 }
