a51952bf42cb058a9d9b2c0434e501bc64dac23d
Gumbers <gustavomarinsuppi@gmail.com>
2020-05-22 20:27:04 -0300

    feat: adding interface to create users and mase sure the developer are not going to make any mistakes with contructors and parameter names or types

--- auth/src/models/user.ts
+++ auth/src/models/user.ts
@@ -1,5 +1,10 @@
 import mongoose from 'mongoose'
 
+interface UserAttrs {
+  email: string
+  password: string
+}
+
 const userSchema = new mongoose.Schema({
   email: {
     type: String,
@@ -12,5 +17,9 @@ const userSchema = new mongoose.Schema({
 })
 
 const User = mongoose.model('User', userSchema)
+
+const builderUser = (attrs : UserAttrs) => {
+  return new User(attrs)
+}
 
 export { User }
