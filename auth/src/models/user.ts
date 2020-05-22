f5f0e8ad773e96ae53e9de81ad62071ea5eba988
Gumbers <gustavomarinsuppi@gmail.com>
2020-05-22 20:35:05 -0300

    chore: changing the build method to a static build methos of the user model

--- auth/src/models/user.ts
+++ auth/src/models/user.ts
@@ -1,25 +1,28 @@
 import mongoose from 'mongoose'
 
 interface UserAttrs {
-  email: string
-  password: string
+	email: string
+	password: string
+}
+
+interface UserModel extends mongoose.Model<any> {
+	build(attrs: UserAttrs): any
 }
 
 const userSchema = new mongoose.Schema({
-  email: {
-    type: String,
-    required : true
-  },
-  password: {
-    type: String,
-    required: true
-  }
+	email: {
+		type: String,
+		required: true,
+	},
+	password: {
+		type: String,
+		required: true,
+	},
 })
 
-const User = mongoose.model('User', userSchema)
-
-const builderUser = (attrs : UserAttrs) => {
-  return new User(attrs)
+userSchema.statics.build = (attrs: UserAttrs) => {
+	return new User(attrs)
 }
+const User = mongoose.model<any,UserModel>('User', userSchema)
 
 export { User }
