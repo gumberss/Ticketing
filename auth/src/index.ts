441507ca1f3ae92a96e4564dd50cdcddaa8b683f
Gumbers <gustavomarinsuppi@gmail.com>
2020-05-21 21:26:42 -0300

    feat: adding mongo db connection

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,6 +1,7 @@
 import express from 'express'
 import { json } from 'body-parser'
 import 'express-async-errors'
+import mongoose from 'mongoose'
 
 import { currentUserRouter } from './routes/current-user'
 import { signinRouter } from './routes/signin'
@@ -8,7 +9,7 @@ import { signoutRouter } from './routes/signout'
 import { signupRouter } from './routes/signup'
 
 import { errorHandler } from './middlewares/error-handler'
-import {NotFoundError} from './errors/not-found-error'
+import { NotFoundError } from './errors/not-found-error'
 
 const app = express()
 app.use(json())
@@ -19,9 +20,24 @@ app.use(signoutRouter)
 app.use(signupRouter)
 
 app.all('*', async (req, res) => {
-  throw new NotFoundError()
+	throw new NotFoundError()
 })
 
 app.use(errorHandler)
 
-app.listen(3000, () => console.log('Listening on port 3000!!'))
+const start = async () => {
+	try {
+		await mongoose.connect('mongodb://auth-mongo-srv:27017/auth', {
+			useNewUrlParser: true,
+			useUnifiedTopology: true,
+			useCreateIndex: true,
+    })
+    console.log('Connected to database')
+	} catch (err) {
+		console.log(err)
+  }
+  app.listen(3000, () => console.log('Listening on port 3000!!!'))  
+}
+
+start()
+
