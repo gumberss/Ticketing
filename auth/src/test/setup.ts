6f6a7cac5b92916100d1e1427c0f729420637d7e
Gumbers <gustavomarinsuppi@gmail.com>
2020-05-26 22:10:52 -0300

    test: adding current user test and refactor signin to a global function (this is only a test, so I want to see if it is a good approach for the next time)

--- auth/src/test/setup.ts
+++ auth/src/test/setup.ts
@@ -1,13 +1,23 @@
 import { MongoMemoryServer } from 'mongodb-memory-server'
 import mongoose from 'mongoose'
+import request from 'supertest'
+import { app } from '../app'
+
+declare global {
+	namespace NodeJS {
+		interface Global {
+			signup(): Promise<string[]>
+		}
+	}
+}
 
 let mongo: any
 
 beforeAll(async () => {
+	process.env.JWT_KEY = 'lalapo'
+	process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0'
 
-  process.env.JWT_KEY = "lalapo"
 	mongo = new MongoMemoryServer()
-
 	const mongoUri = await mongo.getUri()
 
 	await mongoose.connect(mongoUri, {
@@ -28,3 +38,23 @@ afterAll(async () => {
 	await mongo.stop()
 	await mongoose.connection.close()
 })
+
+// probably it is not the best way I think the best way is saparate
+// this code in another file and import it in the test files that 
+//require it, but I'm just making a test
+global.signup = async () => {
+	const email = 'test@test.com'
+	const password = 'password'
+
+	const response = await request(app)
+		.post('/api/users/signup')
+		.send({
+			email,
+			password,
+		})
+		.expect(201)
+
+	const cookie = response.get('Set-Cookie')
+
+	return cookie
+}
