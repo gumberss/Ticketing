2ab2708847f9b55202f1af4af2b7c5c8b80310f6
Gumbers <gustavomarinsuppi@gmail.com>
2020-05-26 21:12:55 -0300

    test: create some more tests about signup route

--- auth/src/routes/__test__/signup.test.ts
+++ auth/src/routes/__test__/signup.test.ts
@@ -2,11 +2,35 @@ import request from 'supertest'
 import { app } from '../../app'
 
 it('returns a 201 on successful signup', async () => {
-  return request(app)
-  .post('/api/users/signup')
-  .send({
-    email: 'test@test.com',
-    password: 'batman-123'
-  })
-  .expect(201)
+	return request(app)
+		.post('/api/users/signup')
+		.send({
+			email: 'test@test.com',
+			password: 'batman-123',
+		})
+		.expect(201)
+})
+
+it('returns a 400 with an invalid email', async () => {
+	return request(app)
+		.post('/api/users/signup')
+		.send({
+			email: 'testcom',
+			password: 'batman-123',
+		})
+		.expect(400)
+})
+
+it('returns a 400 with an invalid password', async () => {
+	return request(app)
+		.post('/api/users/signup')
+		.send({
+			email: 'test@test.com',
+			password: 'b',
+		})
+		.expect(400)
+})
+
+it('returns a 400 with missing email and password', async () => {
+	return request(app).post('/api/users/signup').send({}).expect(400)
 })
