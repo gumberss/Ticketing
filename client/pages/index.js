0980010c9fc6cb43141f2d687d1a6a22c7e2e79b
Gumbers <gustavomarinsuppi@gmail.com>
2020-05-29 21:14:01 -0300

    feat: 1 - Adding method getInitialProps, this method allow us to make requests from the server side (in same cases). 2 - inside getInitialProps, we call nginx from browser (using path) and from server (using a hard sintax to make kubernetes understand that we want to call other container.) 3 - To call to other container, we need to specify the Host property in header of the request, to make sure xginx can understand which host it will apply the rule

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,3 +1,30 @@
-export default () => {
-  return <h1>Hi :)</h1>
+import axios from 'axios'
+
+const LandingPage = ({ currentUser }) => {
+	console.log(currentUser)
+	return <h1>Hi :)</h1>
 }
+
+// This method from Next.js allow us to fetch some data during the server side process
+LandingPage.getInitialProps = async () => {
+	//on server
+	if (typeof window === 'undefined') {
+		const { data } = await axios.get(
+			'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local/api/users/currentuser',
+			//http://KUBERNET-NAMESPACE.KUBERNET-SERVICE.svc.cluster.local/PATH
+			{
+				headers: {
+					Host: 'ticketing.dev',
+				},
+			}
+		)
+		return data
+	}
+	//on broser
+	else {
+		const { data } = await axios.get('/api/users/currentuser')
+		return data
+	}
+}
+
+export default LandingPage
