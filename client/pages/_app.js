aaefb371aeeb279a86306728a0346aa0e09b1caa
Gumbers <gustavomarinsuppi@gmail.com>
2020-05-30 09:11:55 -0300

    refactor: Get current user in the AppComponent to make sure we can access the user from any page we create

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -1,5 +1,28 @@
 import 'bootstrap/dist/css/bootstrap.css'
+import buildClient from '../api/build-client'
 
-export default ({ Component, pageProps }) => {
-  return <Component {...pageProps}/>
+const AppComponent = ({ Component, pageProps, currentUser }) => {
+	return (
+		<>
+			<Component {...pageProps} currentUser/>
+		</>
+	)
 }
+// This method from Next.js allow us to fetch some data during the server side process
+AppComponent.getInitialProps = async ({ Component, ctx }) => {
+	const client = buildClient(ctx)
+
+	const { data } = await client.get('/api/users/currentuser')
+
+	let pageProps = {}
+	if (Component.getInitialProps) {
+		pageProps = await Component.getInitialProps(ctx)
+	}
+
+	return {
+    pageProps,
+    ...data
+  }
+}
+
+export default AppComponent
--- client/pages/index.js
+++ client/pages/index.js
@@ -7,14 +7,4 @@ const LandingPage = ({ currentUser }) => {
 		<h1>You are NOT signed in</h1>
 	)
 }
-
-// This method from Next.js allow us to fetch some data during the server side process
-LandingPage.getInitialProps = async context => {
-	const client = buildClient(context)
-
-	const { data } = await client.get('/api/users/currentuser')
-
-	return data
-}
-
 export default LandingPage
