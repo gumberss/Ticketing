e62c02913334ef1f0ae2eb30abb234196c64af25
Gumbers <gustavomarinsuppi@gmail.com>
2020-06-01 22:00:57 -0300

    feat: adding auth validation to tickets microservice

--- tickets/src/app.ts
+++ tickets/src/app.ts
@@ -2,7 +2,7 @@ import express from 'express'
 import { json } from 'body-parser'
 import 'express-async-errors'
 import cookieSession from 'cookie-session'
-import { errorHandler, NotFoundError } from '@gtickets/common'
+import { errorHandler, NotFoundError, currentUser } from '@gtickets/common'
 import { createTicketRouter } from './routes/new'
 
 
@@ -15,6 +15,8 @@ app.use(
 		secure: process.env.NODE_ENV !== 'test', //share cookies only with https connections
 	})
 )
+
+app.use(currentUser)
 
 app.use(createTicketRouter)
 
