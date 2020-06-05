3c6f04cd104b834e3eda2a454af9ec8fcd51c886
Gumbers <gustavomarinsuppi@gmail.com>
2020-06-04 22:21:36 -0300

    fix: toJson ticket methos to serialize correct id property. test: created tests for show tickets route. feat: create show route

--- tickets/src/app.ts
+++ tickets/src/app.ts
@@ -4,9 +4,10 @@ import 'express-async-errors'
 import cookieSession from 'cookie-session'
 import { errorHandler, NotFoundError, currentUser } from '@gtickets/common'
 import { createTicketRouter } from './routes/new'
-
+import { showTicketRouter } from './routes/show'
 
 const app = express()
+
 app.set('trust proxy', true) // nginx proxy the connection
 app.use(json())
 app.use(
@@ -19,6 +20,7 @@ app.use(
 app.use(currentUser)
 
 app.use(createTicketRouter)
+app.use(showTicketRouter)
 
 app.all('*', async (req, res) => {
 	throw new NotFoundError()
