0bb0c6eaafc230e3b004ed41a698a9baf5770bf9
Gumbers <gustavomarinsuppi@gmail.com>
2020-06-08 21:47:14 -0300

    feat: adding nats client getter and use ticket publish method

--- tickets/src/routes/new.ts
+++ tickets/src/routes/new.ts
@@ -3,6 +3,7 @@ import { requireAuth, validateRequest } from '@gtickets/common'
 import { body } from 'express-validator'
 import { Ticket } from '../models/ticket'
 import { TicketCreatedPublisher } from '../events/publishers/ticket-created-publisher'
+import { natsWrapper } from '../nats-wrapper'
 
 const router = express.Router()
 
@@ -28,6 +29,12 @@ router.post(
 
 		await ticket.save()
 
+		await new TicketCreatedPublisher(natsWrapper.client).publish({
+			id: ticket.id,
+			title: ticket.title,
+			price: ticket.price,
+			userId: ticket.userId
+		})
 
 		res.status(201).send(ticket)
 	}
