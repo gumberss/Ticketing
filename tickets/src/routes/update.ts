618e1e915af336428dabf46837cb6cc3c6bf25ca
Gumbers <gustavomarinsuppi@gmail.com>
2020-06-08 22:12:30 -0300

    feat: publish an event when a ticket is updated

--- tickets/src/routes/update.ts
+++ tickets/src/routes/update.ts
@@ -7,6 +7,8 @@ import {
 } from '@gtickets/common'
 import { Ticket } from '../models/ticket'
 import { body } from 'express-validator'
+import { TicketUpdatedPublisher } from '../events/publishers/ticket-updated-publisher'
+import { natsWrapper } from '../nats-wrapper'
 
 const router = express.Router()
 
@@ -36,9 +38,16 @@ router.put(
 		ticket.set({
 			title,
 			price,
-    })
-    
-    await ticket.save()
+		})
+
+		await ticket.save()
+
+		await new TicketUpdatedPublisher(natsWrapper.client).publish({
+			id: ticket.id,
+			title: ticket.title,
+			price: ticket.price,
+			userId: ticket.userId,
+		})
 
 		res.send(ticket)
 	}
