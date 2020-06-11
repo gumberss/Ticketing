87f741d41870161012504dc8e98ea4f31b4bcf0e
Gumbers <gustavomarinsuppi@gmail.com>
2020-06-11 19:34:01 -0300

    feat: get ticket from database to create an order to it

--- orders/src/routes/new.ts
+++ orders/src/routes/new.ts
@@ -1,7 +1,9 @@
 import mongoose from 'mongoose'
 import express, { Request, Response } from 'express'
-import { requireAuth, validateRequest } from '@gtickets/common'
+import { requireAuth, validateRequest, NotFoundError } from '@gtickets/common'
 import { body } from 'express-validator'
+import { Order } from '../models/order'
+import { Ticket } from '../models/ticket'
 
 const router = express.Router()
 
@@ -9,26 +11,34 @@ router.post(
 	'/api/orders',
 	requireAuth,
 	[
-    body('ticketId')
-    .not()
-    .isEmpty()
-    /**
-     * This validation needs to be carreful, because 
-     * when we make this validation, and the database
-     * of the ticket microservice changes, this validation
-     * will broken, thus, every time you change ticket 
-     * microsservice, you need to check if that change
-     * doesn't impact in this microservice, and it is not
-     * a good aproach. I'll make this validation because 
-     * I'ma hundread percent sure that ticketing microservice 
-     * will use mongo database (learning project) by the end 
-     * of the project.
-     */
-    .custom((input: string) => mongoose.Types.ObjectId.isValid(input))
-    .withMessage('TicketId must be provided')
-  ],
-  validateRequest,
+		body('ticketId')
+			.not()
+			.isEmpty()
+			/**
+			 * This validation needs to be carreful, because
+			 * when we make this validation, and the database
+			 * of the ticket microservice changes, this validation
+			 * will broken, thus, every time you change ticket
+			 * microsservice, you need to check if that change
+			 * doesn't impact in this microservice, and it is not
+			 * a good aproach. I'll make this validation because
+			 * I'ma hundread percent sure that ticketing microservice
+			 * will use mongo database (learning project) by the end
+			 * of the project.
+			 */
+			.custom((input: string) => mongoose.Types.ObjectId.isValid(input))
+			.withMessage('TicketId must be provided'),
+	],
+	validateRequest,
 	async (req: Request, res: Response) => {
+    const { ticketId } = req.body
+    
+    const ticket = await Ticket.findById(ticketId)
+
+    if(!ticket){
+      throw new NotFoundError()
+    }
+
 		res.send({})
 	}
 )
