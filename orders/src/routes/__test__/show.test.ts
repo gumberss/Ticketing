e09696b062cf04be6f083b22e3b94d4047a3d0a3
Gumbers <gustavomarinsuppi@gmail.com>
2020-06-12 14:12:17 -0300

    feat: adding show route content and its test

---
+++ orders/src/routes/__test__/show.test.ts
@@ -0,0 +1,49 @@
+import request from 'supertest'
+import { app } from '../../app'
+import { Ticket } from '../../models/ticket'
+
+it('Fetches the order', async () => {
+	const ticket = Ticket.build({
+		title: 'Concert',
+		price: 100,
+	})
+	await ticket.save()
+
+	const user = global.signup()
+
+	const { body: order } = await request(app)
+		.post('/api/orders')
+		.set('Cookie', user)
+		.send({ ticketId: ticket.id })
+		.expect(201)
+
+	const { body: fetchOrder } = await request(app)
+		.get(`/api/orders/${order.id}`)
+		.set('Cookie', user)
+		.send()
+    .expect(200)
+    
+    expect(fetchOrder.id).toEqual(order.id)
+})
+
+it('Returns an error if one user tries to fetch another users order', async () => {
+	const ticket = Ticket.build({
+		title: 'Concert',
+		price: 100,
+	})
+	await ticket.save()
+
+	const user = global.signup()
+
+	const { body: order } = await request(app)
+		.post('/api/orders')
+		.set('Cookie', user)
+		.send({ ticketId: ticket.id })
+		.expect(201)
+
+	 await request(app)
+		.get(`/api/orders/${order.id}`)
+		.set('Cookie', global.signup())
+		.send()
+    .expect(401)
+})
