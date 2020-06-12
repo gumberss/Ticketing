bc925151a8b6a5e8b26e86840ed5a51249ed7615
Gumbers <gustavomarinsuppi@gmail.com>
2020-06-12 20:01:59 -0300

    feat: making sure the ticket id in ticket microservice is the same as the order microservice

--- orders/src/events/listeners/ticket-created-listener.ts
+++ orders/src/events/listeners/ticket-created-listener.ts
@@ -1,13 +1,29 @@
-import { Listener, TicketCreatedEvent, Subjects, TicketCreatedEventData } from "@gtickets/nats-common";
-import { Message } from "node-nats-streaming";
+import {
+	Listener,
+	TicketCreatedEvent,
+	Subjects,
+	TicketCreatedEventData,
+} from '@gtickets/nats-common'
+import { Message } from 'node-nats-streaming'
+import { queueGroupName } from './queue-group-name'
+import { Ticket } from '../../models/ticket'
 
+export class TicketCreatedListener extends Listener<TicketCreatedEvent> {
+	readonly subject = Subjects.TicketCreated
 
-export class TicketCreatedListener extends Listener<TicketCreatedEvent>{
-  readonly subject = Subjects.TicketCreated;
-  
-  queueGroupName: string = "orders-service"
+	queueGroupName: string = queueGroupName
 
-  onMessage(data: TicketCreatedEventData, msg: Message): void {
-      
-  }
+	async  onMessage(data: TicketCreatedEventData, msg: Message): void {
+		const { id, title, price } = data
+
+		const ticket = Ticket.build({
+      id,
+			title,
+			price,
+		})
+
+		await ticket.save()
+
+		msg.ack()
+	}
 }
