30da1da84ce024988efb1c980968e6757732b570
Gumbers <gustavomarinsuppi@gmail.com>
2020-06-12 14:23:15 -0300

    feat: adding delete route content and a very important comment about the correct api method and why this route shouldn't be called delete

--- orders/src/routes/delete.ts
+++ orders/src/routes/delete.ts
@@ -1,9 +1,35 @@
 import express, { Request, Response } from 'express'
+import { requireAuth, NotFoundError, NotAuthorizedError } from '@gtickets/common'
+import { Order, OrderStatus } from '../models/order'
 
 const router = express.Router()
 
-router.delete('/api/orders/:orderId', async (req: Request, res: Response) => {
-	res.send({})
-})
+/**
+ * Delete is not the correct method, because 
+ * we're actually not deleting the data, we 
+ * are updating it. Probably the put or patch 
+ * method fit better
+ */
+router.delete(
+	'/api/orders/:orderId',
+	requireAuth,
+	async (req: Request, res: Response) => {
+		const { orderId } = req.params
+
+		const order = await Order.findById(orderId)
+
+		if(!order){
+			throw new NotFoundError()
+		}
+
+		if(order.userId !== req.currentUser!.id){
+			throw new NotAuthorizedError()
+		}
+
+		order.status = OrderStatus.Cancelled
+
+		res.send(order)
+	}
+)
 
 export { router as deleteOrderRouter }
