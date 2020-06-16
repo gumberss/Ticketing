e4870b97b4baf23faa557146833311d068e5909f
Gumbers <gustavomarinsuppi@gmail.com>
2020-06-16 20:01:59 -0300

    test: adding some tests to order created listener tests

--- tickets/src/events/listeners/__test__/order-created-listener.test.ts
+++ tickets/src/events/listeners/__test__/order-created-listener.test.ts
@@ -34,3 +34,21 @@ const setup = async () => {
 
 	return { listener, ticket, data, msg }
 }
+
+it('Sets the userId of the ticket', async () => {
+	const { listener, ticket, data, msg } = await setup()
+
+	await listener.onMessage(data, msg)
+
+	const updatedTicket = await Ticket.findById(ticket.id)
+
+	expect(updatedTicket!.orderId).toEqual(data.id)
+})
+
+it('acks the message', async () => {
+  const { listener, data, msg } = await setup()
+
+	await listener.onMessage(data, msg)
+
+  expect(msg.ack).toHaveBeenCalled()
+})
