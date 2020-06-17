847ac73c7945567f31b2be254a559888731e5ad4
Gumbers <gustavomarinsuppi@gmail.com>
2020-06-16 21:01:55 -0300

    feat: adding order cancelled listener

---
+++ tickets/src/events/listeners/order-cancelled-listener.ts
@@ -0,0 +1,39 @@
+import {
+	Listener,
+	OrderCancelledEvent,
+	OrderCreatedEvent,
+	Subjects,
+	OrderStatus,
+} from '@gtickets/nats-common'
+import { Message } from 'node-nats-streaming'
+import { queueGroupName } from './queue-group-name'
+import { Ticket } from '../../models/ticket'
+import { TicketUpdatedPublisher } from '../publishers/ticket-updated-publisher'
+
+export class OrderCancelledListener extends Listener<OrderCreatedEvent> {
+	readonly subject = Subjects.OrderCreated
+	queueGroupName: string = queueGroupName
+
+	async onMessage(data: OrderCancelledEvent['data'], msg: Message) {
+		const ticket = await Ticket.findById(data.ticket.id)
+
+		if (!ticket) {
+			throw new Error('Ticket not found')
+		}
+
+		//ts '?' check dont work very well with null, so we will update with undefined
+		ticket.set({ orderId: undefined })
+
+		await ticket.save()
+		await new TicketUpdatedPublisher(this.client).publish({
+      id: ticket.id,
+			userId: ticket.userId,
+			version: ticket.version,
+			orderId: ticket.orderId,
+			price: ticket.price,
+			title: ticket.title
+    })
+
+    msg.ack()
+	}
+}
