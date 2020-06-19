665117f03f89158a5a25960dd686512bf95db8d7
Gumbers <gustavomarinsuppi@gmail.com>
2020-06-18 22:19:46 -0300

    feat: created the expiration queue first implementation and publish an job to bull queue when an order created event is received

--- expiration/src/events/listeners/order-created-listener.ts
+++ expiration/src/events/listeners/order-created-listener.ts
@@ -6,11 +6,16 @@ import {
 } from '@gtickets/nats-common'
 import { Message } from 'node-nats-streaming'
 import { queueGroupName } from './queue-group-name'
+import { expirationQueue } from '../../queues/expiration-queue'
 
 export class OrderCreatedListener extends Listener<OrderCreatedEvent> {
 	readonly subject = Subjects.OrderCreated
 	queueGroupName = queueGroupName
-	async onMessage(data: OrderCreatedEvent['data']) {
-		throw new Error('Method not implemented.')
+	async onMessage(data: OrderCreatedEvent['data'], msg: Message) {
+		await expirationQueue.add({
+			orderId: data.id,
+    })
+    
+    msg.ack()
 	}
 }
--- expiration/src/index.ts
+++ expiration/src/index.ts
@@ -1,4 +1,5 @@
 import { natsWrapper } from './nats-wrapper'
+import { OrderCreatedListener } from './events/listeners/order-created-listener'
 
 const start = async () => {
 
@@ -29,6 +30,7 @@ const start = async () => {
 		process.on('SIGINT', () => natsWrapper.client.close())
 		process.on('SIGTERM', () => natsWrapper.client.close())
 
+		new OrderCreatedListener(natsWrapper.client).listen()
 
 		console.log('Connected to database')
 	} catch (err) {
