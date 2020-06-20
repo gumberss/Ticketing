b0b09f5e0f4ca6b021990c211125ee4beeb16481
Gumbers <gustavomarinsuppi@gmail.com>
2020-06-20 14:53:41 -0300

    feat: adding new payment route

--- payments/src/app.ts
+++ payments/src/app.ts
@@ -3,7 +3,7 @@ import { json } from 'body-parser'
 import 'express-async-errors'
 import cookieSession from 'cookie-session'
 import { errorHandler, NotFoundError, currentUser } from '@gtickets/common'
-
+import { createChargeRouter } from './routes/new'
 
 const app = express()
 
@@ -17,8 +17,7 @@ app.use(
 )
 
 app.use(currentUser)
-
-
+app.use(createChargeRouter)
 
 app.all('*', async (req, res) => {
 	throw new NotFoundError()
