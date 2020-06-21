fb15fb24b4844a1ab446ee391035e9c764052d0c
Gumbers <gustavomarinsuppi@gmail.com>
2020-06-21 14:44:01 -0300

    feat: publishing a payment created event with payment data

--- payments/src/routes/new.ts
+++ payments/src/routes/new.ts
@@ -12,6 +12,8 @@ import { Order } from '../models/orders'
 import { OrderStatus } from '@gtickets/nats-common'
 import { stripe } from '../stripe'
 import { Payment } from '../models/payment'
+import { PaymentCreatedPublisher } from '../events/publishers/payment-created-publisher'
+import { natsWrapper } from '../nats-wrapper'
 
 const router = express.Router()
 
@@ -36,19 +38,25 @@ router.post(
 		if (order.status === OrderStatus.Cancelled) {
 			throw new BadRequestError('Cannot pay for an cancelled order')
 		}
-		
+
 		const charge = await stripe.charges.create({
 			currency: 'usd',
 			amount: order.price * 100, //cents
-			source: token
+			source: token,
 		})
 
 		const payment = Payment.build({
 			orderId,
-			stripeId: charge.id
+			stripeId: charge.id,
 		})
 
 		await payment.save()
+
+		await new PaymentCreatedPublisher(natsWrapper.client).publish({
+			id: payment.id,
+			orderId: payment.orderId,
+			stripeId: payment.stripeId,
+		})
 
 		res.status(201).send({ success: true })
 	}
