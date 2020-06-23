eb6b3e408524351ed5722c0b318e600a394b61cb
Gumbers <gustavomarinsuppi@gmail.com>
2020-06-22 21:58:02 -0300

    feat: adding time expires control

--- client/pages/orders/[orderId].js
+++ client/pages/orders/[orderId].js
@@ -1,15 +1,37 @@
-const OrderShow = ({ order }) => {
-	const milisecondsLeft = new Date(order.expiresAt) - new Date()
+import { useEffect, useState } from 'react'
 
-	return <div>{milisecondsLeft / 1000} seconds until order expires</div>
+const OrderShow = ({ order }) => {
+	const [timeLeft, setTimeleft] = useState('')
+
+	useEffect(() => {
+		const findTimeLeft = () => {
+			const milisecondsLeft = new Date(order.expiresAt) - new Date()
+
+      setTimeleft(Math.round(milisecondsLeft / 1000))
+    }
+
+    findTimeLeft()
+
+    const timerId = setInterval(findTimeLeft, 1000)
+
+    /**
+     * When a function is returned from UseEffect, that function will be 
+     * invoked when you navigate away from this component (always) or if 
+     * the component is going to be rerendered (only if the [] has 
+     * something and it was updated).
+     */
+    return () => {
+      clearInterval(timerId)
+    }
+	}, [])
+
+	return <div>Time left to pay: {timeLeft} seconds</div>
 }
 
 OrderShow.getInitialProps = async (context, client) => {
 	const { orderId } = context.query
 
 	const { data } = await client.get(`/api/orders/${orderId}`)
-
-  console.log('h1')
 
 	return { order: data }
 }
