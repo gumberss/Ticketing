cfb9e0749bd5e22a0802a45d6aaf3829a3dd0572
Gumbers <gustavomarinsuppi@gmail.com>
2020-06-23 19:24:51 -0300

    feat: payment an order with stripe

--- client/pages/orders/[orderId].js
+++ client/pages/orders/[orderId].js
@@ -1,35 +1,46 @@
 import { useEffect, useState } from 'react'
+import StripeCheckout from 'react-stripe-checkout'
 
-const OrderShow = ({ order }) => {
+const OrderShow = ({ order, currentUser }) => {
 	const [timeLeft, setTimeleft] = useState(0)
 
 	useEffect(() => {
 		const findTimeLeft = () => {
 			const milisecondsLeft = new Date(order.expiresAt) - new Date()
 
-      setTimeleft(Math.round(milisecondsLeft / 1000))
-    }
+			setTimeleft(Math.round(milisecondsLeft / 1000))
+		}
 
-    findTimeLeft()
+		findTimeLeft()
 
-    const timerId = setInterval(findTimeLeft, 1000)
+		const timerId = setInterval(findTimeLeft, 1000)
 
-    /**
-     * When a function is returned from UseEffect, that function will be 
-     * invoked when you navigate away from this component (always) or if 
-     * the component is going to be rerendered (only if the [] has 
-     * something and it was updated).
-     */
-    return () => {
-      clearInterval(timerId)
-    }
+		/**
+		 * When a function is returned from UseEffect, that function will be
+		 * invoked when you navigate away from this component (always) or if
+		 * the component is going to be rerendered (only if the [] has
+		 * something and it was updated).
+		 */
+		return () => {
+			clearInterval(timerId)
+		}
 	}, [])
 
-  if(timeLeft <= 0){
-    return <div> Order expired</div>
-  }
+	if (timeLeft <= 0) {
+		return <div> Order expired</div>
+	}
 
-	return <div>Time left to pay: {timeLeft} seconds</div>
+	return (
+		<div>
+			Time left to pay: {timeLeft} seconds
+			<StripeCheckout
+				token={token => console.log(token)}
+				stripeKey={process.env.STRIPE_PUBLIC_KEY}
+				amount={order.ticket.price * 100}
+				email={currentUser.email}
+			/>
+		</div>
+	)
 }
 
 OrderShow.getInitialProps = async (context, client) => {
