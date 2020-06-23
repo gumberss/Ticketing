7dfeac6547b234894bb85cb36512817197628d5c
Gumbers <gustavomarinsuppi@gmail.com>
2020-06-22 21:12:39 -0300

    feat: creating the ticket detail page

--- client/pages/tickets/[ticketId].js
+++ client/pages/tickets/[ticketId].js
@@ -1,4 +1,36 @@
-const TicketShow = () => {
-  return <div>Ticket Show</div>
+import useRequest from '../../hooks/use-request'
+
+const TicketShow = ({ ticket }) => {
+	const { doRequest, errors } = useRequest({
+		url: '/api/orders',
+		method: 'post',
+		onSuccess: order => console.log(order),
+	})
+
+	const onPurchase = () => {
+		doRequest({
+			ticketId: ticket.id,
+		})
+	}
+
+	return (
+		<div>
+			<h1>{ticket.title}</h1>
+			<h4>Price: {ticket.price}</h4>
+			<button onClick={onPurchase} className="btn btn-primary">
+				{' '}
+				Purchase
+			</button>
+			{errors}
+		</div>
+	)
 }
+TicketShow.getInitialProps = async (context, client) => {
+	const { ticketId } = context.query
+
+	const { data } = await client.get(`/api/tickets/${ticketId}`)
+
+	return { ticket: data }
+}
+
 export default TicketShow
